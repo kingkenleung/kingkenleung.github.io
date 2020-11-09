cc4f87bd25f26be86de646175e8f46453c6a3939
kingkenleung <30319344+kingkenleung@users.noreply.github.com>
2020-11-09 13:31:26 +0800

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -18,14 +18,14 @@
 
 Three.Vector3(x,y,z)
 
-	Forward is positive
-					^
-					|
-					|
+    Forward is positive
+                    ^
+                    |
+                    |
 x: along axis of lens
-					|
-					|
-					v
+                    |
+                    |
+                    v
  Backward is negative
 
 y: vertical axis
@@ -40,46 +40,90 @@ Left is positive <--- z: perpendicular to axis of lens ---> Right is negative
 
 //Inventory Table: 
 //https://docs.google.com/spreadsheets/d/119Pk9acU5favxD0pep7ospn2rLY_mHyDWzQSoFurpWA/edit#gid=0
-const scenes = [
-  { //id:0
-    path: 'pic/LT_1.JPG',
-    initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
-    infospots: [
-      {
-        position: [-150, 0, 250],
-				hoverVideo: 'https://www.youtube.com/embed/A9cr7IFqnxk',
-        hoverText: 'Shatin Pui Ying College',
-        link: '',
-        icon: PANOLENS.DataImage.Play
-      },
-			{
-        position: [-150, 0, -250],
-				hoverVideo: 'https://www.youtube.com/embed/dGBsOLnjDxM',
-        hoverText: 'Shatin Pui Ying College 2',
-        link: '',
-        icon: PANOLENS.DataImage.Play
-      },
-    ],
-		sceneLinkage: [
-			{id: 1, direction: new THREE.Vector3(-1200,-560,0)},
-		]
-  },
 
-  { //id:1
-    path: 'pic/LT_2.JPG',
-    initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
-    infospots: [
-      {
-        position: [400, 0, 0],
-				hoverVideo: 'https://www.youtube.com/embed/ZL0DU4_pcpQ',
-        hoverText: 'Shatin Pui Ying College',
-        link: '',
-        icon: PANOLENS.DataImage.Play
-      },
-    ],
-		sceneLinkage: [
-			{id: 0, direction: new THREE.Vector3(400, -300, 1000)},
-		]
-  },
-	
-]
+// const scenes = [
+//   { //id:0
+//     path: 'pic/LT_1.JPG',
+//     initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
+//     infospots: [
+//       {
+//         position: [-150, 0, 250],
+// 				hoverVideo: 'https://www.youtube.com/embed/A9cr7IFqnxk',
+//         hoverText: 'Shatin Pui Ying College',
+//         link: '',
+//         icon: PANOLENS.DataImage.Play
+//       },
+// 			{
+//         position: [-150, 0, -250],
+// 				hoverVideo: 'https://www.youtube.com/embed/dGBsOLnjDxM',
+//         hoverText: 'Shatin Pui Ying College 2',
+//         link: '',
+//         icon: PANOLENS.DataImage.Play
+//       },
+//     ],
+// 		sceneLinkage: [
+// 			{id: 1, direction: new THREE.Vector3(-1200,-560,0)},
+// 		]
+//   },
+
+//   { //id:1
+//     path: 'pic/LT_2.JPG',
+//     initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
+//     infospots: [
+//       {
+//         position: [400, 0, 0],
+// 				hoverVideo: 'https://www.youtube.com/embed/ZL0DU4_pcpQ',
+//         hoverText: 'Shatin Pui Ying College',
+//         link: '',
+//         icon: PANOLENS.DataImage.Play
+//       },
+//     ],
+// 		sceneLinkage: [
+// 			{id: 0, direction: new THREE.Vector3(400, -300, 1000)},
+// 		]
+//   },
+
+// ]
+const scenes = {
+    lt1: { //id:lt1
+        path: 'pic/LT_1.JPG',
+        initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
+        infospots: [
+            {
+                position: [-150, 0, 250],
+                hoverVideo: 'https://www.youtube.com/embed/A9cr7IFqnxk',
+                hoverText: 'Shatin Pui Ying College',
+                link: '',
+                icon: PANOLENS.DataImage.Play
+            },
+            {
+                position: [-150, 0, -250],
+                hoverVideo: 'https://www.youtube.com/embed/dGBsOLnjDxM',
+                hoverText: 'Shatin Pui Ying College 2',
+                link: '',
+                icon: PANOLENS.DataImage.Play
+            },
+        ],
+        sceneLinkage: [
+            { id: 'lt2', direction: new THREE.Vector3(-1200, -560, 0) },
+        ]
+    },
+
+    lt2: { //lt2
+        path: 'pic/LT_2.JPG',
+        initialLookAtPosition: new THREE.Vector3(-1500, 0, 0),
+        infospots: [
+            {
+                position: [400, 0, 0],
+                hoverVideo: 'https://www.youtube.com/embed/ZL0DU4_pcpQ',
+                hoverText: 'Shatin Pui Ying College',
+                link: '',
+                icon: PANOLENS.DataImage.Play
+            },
+        ],
+        sceneLinkage: [
+            { id: 'lt1', direction: new THREE.Vector3(400, -300, 1000) },
+        ]
+    },
+
+}
